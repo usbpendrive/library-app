3a4e14618237d625ec4eb9b8b335f9fcd5aa728c
usbpendrive <ferry.fernando@gmail.com>
2018-12-21 09:41:43 +0700

    Initialize new odoo module project.

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Library Management Application',
+    'description': 'Library books, members and book borrowing.',
+    'author': 'Fernando',
+    'depends': ['base'],
+    'license': 'LGPL',
+    'data': [
+        'security/library_security.xml',
+        'security/ir.model.access.csv',
+        'views/library_menu.xml',
+        'views/book_view.xml',
+        'views/book_list_template.xml',
+    ],
+    'application': True,
+    'installable': True
+}
