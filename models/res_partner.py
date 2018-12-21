deffc70bb4cc0fdf806c8b49ff995959f8f0b13d
usbpendrive <ferry.fernando@gmail.com>
2018-12-21 15:07:36 +0700

    res_partner error fix.

--- models/res_partner.py
+++ models/res_partner.py
@@ -3,7 +3,6 @@ from odoo import fields, models
 
 class Partner(models.Model):
     _inherit = 'res.partner'
-    _parent_store = True
 
     published_book_ids = fields.One2many(
         'library.book',
